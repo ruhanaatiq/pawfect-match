c2fab678bbdba883c8d198484cae3aa7262bba9b
ruhanaatiq <ruhana.atiq@gmail.com>
2025-09-14 12:34:39 +0600

    added our services section

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,6 +2,7 @@ import Hero from "@/components/Hero";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
 import Image from "next/image";
+import Services from "@/components/Services";
 
 export default function RootLayout({ children }) {
   return (
@@ -13,6 +14,7 @@ export default function RootLayout({ children }) {
 
         <Navbar />
         <Hero></Hero>
+        <Services></Services>
         <main className="mx-auto max-w-6xl px-4">{children}</main>
       </body>
     </html>
